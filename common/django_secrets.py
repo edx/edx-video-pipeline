f7cb3e44026bcd1780d3d06c462708992cd2b4d5
Gregory Martin <greg@edx.org>
2017-07-05 16:45:09 -0400

    debug

--- common/django_secrets.py
+++ common/django_secrets.py
@@ -1,25 +1,25 @@
 """
-edx-video-pipeline's Django Secrets
-NEVER SHARE ANYTHING IN HERE, like, EVER
---assume unchanged in git--
+VEDA's Django Secrets Shim
+
+This acts as a django-secret shimmer until we can finish pushing all changes to terraform/prod
+
 """
-
 import os
+import yaml
 
-DJANGO_SECRET_KEY = ""
+read_yaml = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    'instance_config.yaml'
+)
 
-DJANGO_DB_USER = ""
-DJANGO_DB_PASS = ""
+with open(read_yaml, 'r') as stream:
+    try:
+        return_dict = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        return_dict = None
 
+
+DJANGO_SECRET_KEY = return_dict['django_secret_key']
 DJANGO_ADMIN = ('', '')
-
-SANDBOX_TOKEN = None
-
-if SANDBOX_TOKEN is not None and SANDBOX_TOKEN in os.path.dirname(__file__):
-    DEBUG = True
-    DBHOST = ''
-    pipeline_dbname = ""
-else:
-    DEBUG = False
-    DBHOST = ''
-    pipeline_dbname = ""
+DEBUG = True
+DATABASES = return_dict['DATABASES']
