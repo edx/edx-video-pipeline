fe95c4e9d1e6c0072c895436ae4517baf1c4467c
Gregory Martin <greg@edx.org>
2017-07-05 19:35:28 -0400

    update settings

--- control/control_env.py
+++ control/control_env.py
@@ -14,7 +14,7 @@ project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if project_path not in sys.path:
     sys.path.append(project_path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'video_common.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'VEDA.settings'
 
 django.setup()
 
