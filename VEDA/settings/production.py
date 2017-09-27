9d308ff861678a8751058348bc646a50fef9259b
Mushtaq Ali <mushtaque@edx.org>
2017-09-27 18:46:37 +0500

    Add JWT AUTH settings - EDUCATOR-1510

--- VEDA/settings/production.py
+++ VEDA/settings/production.py
@@ -10,7 +10,7 @@ ALLOWED_HOSTS = ['*']
 
 # Keep track of the names of settings that represent dicts. Instead of overriding the values in base.py,
 # the values read from disk should UPDATE the pre-configured dicts.
-DICT_UPDATE_KEYS = ('DATABASES', 'SECRET_KEY', 'STATIC_ROOT_PATH')
+DICT_UPDATE_KEYS = ('DATABASES', 'SECRET_KEY', 'STATIC_ROOT_PATH', 'JWT_AUTH')
 
 CONFIG_DATA = get_config()
 # Remove the items that should be used to update dicts, and apply them separately rather
