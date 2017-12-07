85e77f11586783125366dd0d4e22cf00979ad1c5
muhammad-ammar <mammar@gmail.com>
2017-12-07 12:50:43 +0500

    add service variant name

--- VEDA/settings/production.py
+++ VEDA/settings/production.py
@@ -7,16 +7,18 @@ from VEDA.settings.utils import get_logger_config
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+DEFATULT_SERVICE_VARIANT_NAME = 'video-pipeline'
 
 ALLOWED_HOSTS = ['*']
 
-LOGGING = get_logger_config()
+CONFIG_DATA = get_config()
+
+LOGGING = get_logger_config(service_variant=CONFIG_DATA.get('SERVICE_VARIANT_NAME', DEFATULT_SERVICE_VARIANT_NAME))
 
 # Keep track of the names of settings that represent dicts. Instead of overriding the values in base.py,
 # the values read from disk should UPDATE the pre-configured dicts.
 DICT_UPDATE_KEYS = ('DATABASES', 'JWT_AUTH')
 
-CONFIG_DATA = get_config()
 # Remove the items that should be used to update dicts, and apply them separately rather
 # than pumping them into the local vars.
 dict_updates = {key: CONFIG_DATA.pop(key, None) for key in DICT_UPDATE_KEYS}
