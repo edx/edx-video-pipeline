f594e547e6e2e979a2519a51440d28538f2dd527
Alan Zarembok <azarembok@edx.org>
2019-10-15 14:28:54 -0400

    Fix string/byte handling for python3 compatibility.

--- control/veda_utils.py
+++ control/veda_utils.py
@@ -62,43 +62,6 @@ class Output(object):
         duration_seconds = (((hours * 60) + minutes) * 60) + seconds
         return duration_seconds
 
-    @staticmethod
-    def status_bar(process):
-        """
-        This is a little gross, but it'll get us a status bar thingy
-
-        """
-        fps = None
-        duration = None
-        while True:
-            line = process.stdout.readline().strip()
-
-            if line == '' and process.poll() is not None:
-                break
-            if fps is None or duration is None:
-                if "Stream #" in line and " Video: " in line:
-                    fps = [s for s in line.split(',') if "fps" in s][0].strip(' fps')
-
-                if "Duration: " in line:
-                    dur = line.split('Duration: ')[1].split(',')[0].strip()
-                    duration = Output()._seconds_from_string(duration=dur)
-
-            else:
-                if 'frame=' in line:
-                    cur_frame = line.split('frame=')[1].split('fps=')[0].strip()
-                    end_frame = float(duration) * float(fps.strip())
-                    pctg = (float(cur_frame) / float(end_frame))
-                    sys.stdout.write('\r')
-                    i = int(pctg * 20.0)
-                    sys.stdout.write("%s : [%-20s] %d%%" % ('Transcode', '=' * i, int(pctg * 100)))
-                    sys.stdout.flush()
-        """
-        Just for politeness
-        """
-        sys.stdout.write('\r')
-        sys.stdout.write("%s : [%-20s] %d%%" % ('Transcode', '=' * 20, 100))
-        sys.stdout.flush()
-
 
 class Report(object):
 
@@ -243,6 +206,7 @@ class Metadata(object):
         p = subprocess.Popen(ff_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
         for line in iter(p.stdout.readline, b''):
+            line = line.decode('utf-8')
             if "Duration: " in line:
                 self.video_proto.duration = line.split(',')[0].split(' ')[-1]
                 try:
