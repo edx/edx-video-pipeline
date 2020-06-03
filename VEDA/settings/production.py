e80608ebd0abb737ae8f3eb1b2be64caf2a185a8
Syed Muhammad Dawoud Sheraz Ali <40599381+DawoudSheraz@users.noreply.github.com>
2020-06-03 18:21:14 +0500

    add JWT_AUTH dict update at production (#219)
    * add JWT_AUTH dict update at production

--- VEDA/settings/production.py
+++ VEDA/settings/production.py
@@ -18,7 +18,7 @@ LOGGING = get_logger_config(service_variant=CONFIG_DATA.get('SERVICE_VARIANT_NAM
 
 # Keep track of the names of settings that represent dicts. Instead of overriding the values in base.py,
 # the values read from disk should UPDATE the pre-configured dicts.
-DICT_UPDATE_KEYS = ('DATABASES',)
+DICT_UPDATE_KEYS = ('DATABASES', 'JWT_AUTH')
 
 # Remove the items that should be used to update dicts, and apply them separately rather
 # than pumping them into the local vars.
